5536fe50d289a5857bbcea7b0147d7db91cee2ea
Saqib <iamsaqib24@gmail.com>
2023-03-12 13:11:49 +0530

    subscribed-to-store

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Home from '../assets/images/home.png'
+import Shorts from '../assets/images/reels.png'
+import Subscribe from '../assets/images/subscribe.png'
+import { useSelector } from 'react-redux'
+
+
 
 function Sidebar() {
+
+  const isHamOpen = useSelector((store) => store.app.isHamOpen)
+
+  if(!isHamOpen) return null
+
   return (
-    <div>Sidebar</div>
+    <div className="w-56 shadow-md p-4">
+      <ul className="">
+        <li className="cursor-pointer p-2"><img className="h-5 mb-1 mr-3 inline" src={Home} alt="home" /> Home</li>
+        <li className="cursor-pointer p-2"><img className="h-5 mb-1 mr-3 inline" src={Shorts} alt="shorts" /> Shorts</li>
+        <li className="cursor-pointer p-2"><img className="h-5 mb-1 mr-3 inline" src={Subscribe} alt="subscribe" /> Subscriptions</li>
+      </ul>
+    </div>
   )
 }
 
