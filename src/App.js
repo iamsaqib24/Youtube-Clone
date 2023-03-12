6182e643823664cb5277497ec6e32972b000fa23
Saqib <iamsaqib24@gmail.com>
2023-03-12 10:42:37 +0530

    setup-done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Challenge accepted, Ashraya</h1>
-      <h2>Setting things up</h2>
+    <div className="text-center mt-10">
+      <h1 className="text-orange-900 font-bold">Challenge accepted, Ashraya</h1>
+      <h2 className="text-orange-700 font-bold">Setting things up</h2>
+      <h3 className="text-orange-500 font-semibold">all configuration done</h3>
     </div>
   );
 }
---
+++ src/const/constant.js
@@ -0,0 +1,1 @@
+const key = "AIzaSyB89pWue4QxgQ_mnivNKk9topC0mzQG4vs"
